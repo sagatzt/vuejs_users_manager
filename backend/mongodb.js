e81a8927a624cc54d74ee09be35b299f477ed679
Awakeboy <57663900+sagatzt@users.noreply.github.com>
2021-03-17 07:40:30 +0100

    Update mongodb.js

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useFindAndModify:false,
 })
 
 module.exports=mongoose.connection

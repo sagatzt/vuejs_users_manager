3ef8cc8617ea95c12975b2d71490b0f790b89474
sagatzt <sagatzt@gmail.com>
2021-03-25 09:13:48 +0100

    ok

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -7,7 +7,7 @@ let data={
   port=process.env.DB_PORT,
   name=process.env.DB_NAME
 }
-
+ 
 mongoose.connect(`mongodb://${data.user}:${data.pass}@${data.host}:${data.port}/${data.name}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
